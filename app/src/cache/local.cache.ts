45f15c7297f395c2497ca3577e9deb2b90e722e0
Sofia Tejerina <sofiatejerina23@gmail.com>
2021-10-12 00:06:08 -0300

    Add ttl (#29)
    * Add ttl
    
    * Refactor
    
    * Refactor
    
    * package
    
    * lock

--- app/src/cache/local.cache.ts
+++ app/src/cache/local.cache.ts
@@ -4,18 +4,30 @@ export default class LocalMemoryDB extends MemoryDBInterface {
     map = {}
 
     async getValue(key: string): Promise<string> {
-        return this.map[key]
+        const obj = this.map[key]
+        if (obj) {
+            return obj['value']
+        }
     }
 
     async setValue(key: string, value: string): Promise<void> {
-        this.map[key] = value
+        this.map[key] = {value, 'ttl': 0}
     }
 
     async setValueWithTTL(key: string, value: string, ttlInMinutes: number): Promise<void> {
-        await this.setValue(key, value)
+        this.map[key] = {value, 'ttl': this.generateTTL(ttlInMinutes)}
     }
 
     async getTTL(key: string): Promise<number> {
-        return 0;
+        const value = this.map[key]
+        if (value) {
+            return value['ttl'] - new Date().getTime();
+        }
+    }
+
+    private generateTTL(ttlDefaultTime: number): number {
+        const createDate = new Date()
+        createDate.setMinutes(createDate.getMinutes() + ttlDefaultTime)
+        return createDate.getTime()
     }
 }
